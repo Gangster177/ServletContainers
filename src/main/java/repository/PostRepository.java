42068bc96edde6da576f62bf8997355986e56487
Nikita Novikov <monstars51@gmail.com>
2023-08-13 22:09:38 +0300

    first commit

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,49 @@
+package repository;
+
+import exception.NotFoundException;
+import model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private final ConcurrentHashMap<Long, Post> posts;
+    private final AtomicLong postID;
+
+    public PostRepository() {
+        posts = new ConcurrentHashMap<>();
+        postID = new AtomicLong(0);
+    }
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long id = postID.incrementAndGet();
+            post.setId(id);
+            posts.put(id,post);
+        } else if (post.getId() != 0) {
+            Long currentId = post.getId();
+            posts.put(currentId, post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+        } else {
+            throw new NotFoundException("Введен неверный id");
+        }
+    }
+}
