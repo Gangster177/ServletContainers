d6825fd221f59309222a31d6fc69461611491ec9
Nikita Novikov <monstars51@gmail.com>
2023-08-16 22:31:34 +0300

    add annotation

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -1,8 +1,7 @@
 package servlet;
 
 import controller.PostController;
-import repository.PostRepository;
-import service.PostService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,9 +17,8 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("");
+        controller = context.getBean(PostController.class);
     }
 
     @Override
